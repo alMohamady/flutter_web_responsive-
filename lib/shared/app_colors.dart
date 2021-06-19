c2a81c07d0f002f4a23591fc1a0ea6b623a6b1ff
al Mohamady <al.Mohamady47@gmail.com>
2021-06-19 20:02:14 +0200

    Lesson 008

---
+++ lib/shared/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color mainColor = Color(0xff48C9F3);
+  static Color bgColor = Color(0xffEEF7FA);
+  static Color bgSideMenu = Color(0xff1A2B30);
+  static Color white = Colors.white;
+  static Color black = Colors.black;
+}
