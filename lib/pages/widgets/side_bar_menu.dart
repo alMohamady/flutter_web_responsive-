bd9539619b6630396f1994103b67f8780ae64457
al Mohamady <al.Mohamady47@gmail.com>
2021-06-19 22:55:06 +0200

    Lesson 010

--- lib/pages/widgets/side_bar_menu.dart
+++ lib/pages/widgets/side_bar_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/shared/app_colors.dart';
 
 class SideBar extends StatefulWidget {
   @override
@@ -8,6 +9,76 @@ class SideBar extends StatefulWidget {
 class _SideBarState extends State<SideBar> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Drawer(
+      elevation: 0,
+      child: Container(
+        color: AppColor.bgSideMenu,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: Text(
+                "MAIN WEB",
+                style: TextStyle(
+                    color: AppColor.mainColor,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            DrawerList(
+              icon: "assets/menu_home.png",
+              title: "Dashbord",
+              press: () {},
+            ),
+            DrawerList(
+              title: "Recruitment",
+              icon: "assets/menu_recruitment.png",
+              press: () {},
+            ),
+            DrawerList(
+              title: "Onboarding",
+              icon: "assets/menu_onboarding.png",
+              press: () {},
+            ),
+            DrawerList(
+              title: "Reports",
+              icon: "assets/menu_report.png",
+              press: () {},
+            ),
+            DrawerList(
+              title: "Calendar",
+              icon: "assets/menu_calendar.png",
+              press: () {},
+            ),
+            DrawerList(
+              title: "Settings",
+              icon: "assets/menu_settings.png",
+              press: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DrawerList extends StatelessWidget {
+  final String title, icon;
+  final VoidCallback press;
+
+  const DrawerList({Key key, this.title, this.icon, this.press})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Image.asset(icon),
+      title: Text(
+        title,
+        style: TextStyle(color: AppColor.white),
+      ),
+      onTap: press,
+    );
   }
 }
