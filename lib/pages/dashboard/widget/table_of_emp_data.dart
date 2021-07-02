d36a7cddb45770ab9bae1e378119f31c2f2e5de2
al Mohamady <al.Mohamady47@gmail.com>
2021-07-02 23:34:39 +0200

    Lesson 014

--- lib/pages/dashboard/widget/table_of_emp_data.dart
+++ lib/pages/dashboard/widget/table_of_emp_data.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/shared/app_colors.dart';
 
 class TableOfEmpData extends StatefulWidget {
   @override
@@ -8,6 +9,66 @@ class TableOfEmpData extends StatefulWidget {
 class _TableOfEmpDataState extends State<TableOfEmpData> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: BoxDecoration(
+        color: AppColor.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      padding: EdgeInsets.all(20),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Emplyees Data",
+                style: TextStyle(
+                  color: AppColor.black,
+                  fontSize: 22,
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: AppColor.mainColor,
+                    borderRadius: BorderRadius.circular(100)),
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                child: Text(
+                  "Go To Table",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: AppColor.black),
+                ),
+              )
+            ],
+          ),
+          Divider(
+            thickness: 0.5,
+            color: Colors.grey,
+          ),
+          Table(
+              defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+              children: [
+                TableRow(
+                    decoration: BoxDecoration(
+                        border: Border(
+                            bottom:
+                                BorderSide(color: Colors.grey, width: 0.5))),
+                    children: [
+                      tableHeader("Emp Name"),
+                      tableHeader("Position"),
+                      tableHeader("Status"),
+                      tableHeader(""),
+                    ]),
+              ])
+        ],
+      ),
+    );
+  }
+
+  Widget tableHeader(text) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 15),
+      child: Text(text,
+          style: TextStyle(fontWeight: FontWeight.bold, color: AppColor.black)),
+    );
   }
 }
