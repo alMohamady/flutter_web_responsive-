50b51775f5d5bfbe09ae70e6fb413e45d289fcf1
al Mohamady <al.Mohamady47@gmail.com>
2021-07-03 02:45:58 +0200

    Lesson 019.1

--- lib/shared/app_responsive.dart
+++ lib/shared/app_responsive.dart
@@ -10,11 +10,21 @@ class AppResponsive extends StatelessWidget {
       : super(key: key);
 
   static bool isMobile(context) => MediaQuery.of(context).size.width < 900;
-  static bool isTablet(context) => MediaQuery.of(context).size.width < 900;
-  static bool isDesktop(context) => MediaQuery.of(context).size.width < 900;
+  static bool isTablet(context) =>
+      MediaQuery.of(context).size.width < 1100 &&
+      MediaQuery.of(context).size.width >= 900;
+  static bool isDesktop(context) => MediaQuery.of(context).size.width >= 1100;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final Size _size = MediaQuery.of(context).size;
+
+    if (isDesktop(context)) {
+      return desktop;
+    } else if (isTablet(context)) {
+      return tablet;
+    } else {
+      return mobile;
+    }
   }
 }
